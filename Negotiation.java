9567ea4a3cd78fb7aed49826cbb3626eae5918dc
Madison <mburke16@amherst.edu>
2014-07-29 10:36:56 -0400

    empty negotiation class

---
+++ Negotiation.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+// The negotiation class is the main class of this app. This is where info is cached, and controls what pages are brought up, and with what information.
+
+public class Negotiation extends JFrame implements ActionListener
+{
+    private Page page; 
+    private boolean loggedIn;
+    private String userName;
+    private String theme;
+    private String[] interests;
+    //private Page page // depending on how we set up the pages
+    
+
+
+
+
+
+
+
+
+
+
+
+}
